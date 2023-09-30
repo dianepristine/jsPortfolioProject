d347801d111772c04aacc0fc83f5cf7422174121
Tyson Morgan <tychi23@aol.com>
2023-09-29 23:33:00 -0700

    feed updates

--- js/feed.js
+++ js/feed.js
@@ -1,5 +1,37 @@
 // Pet will become hungry over time. Implement feeding function to address the need.
 
-function feedPet() {
+// function feedPet() {
     
+// }
+
+var health = 10;
+var elem = document.getElementById('Timer');
+var timerId = setInterval(countdown, 1000);
+
+
+function countdown() {
+
+    if (health < 0) {
+        clearInterval(timerId);
+        alert("Your Tomagotchi has died :(")
+
+    } else {
+        elem.innerHTML = health + ' health status';
+        health--;
+    }
+
+    if (health == 5) {
+        alert("Don't forget to feed your Tomagotchi!")
+    }
+
+}
+
+function addHealth() {
+
+    if (health >= 100) {
+        alert("Your Tomagotchi is full");
+    }
+    else {
+        health = health + 5;
+    }
 }
