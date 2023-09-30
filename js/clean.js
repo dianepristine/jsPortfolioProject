92739546f5a0ebbfbf413245fc7ae1200176b8ff
dianepristine <dianepristine01@gmail.com>
2023-09-29 21:55:22 -0700

    diane's update for cleanPet.js

--- js/clean.js
+++ js/clean.js
@@ -1,5 +1,28 @@
 // Pet may become dirty. Implement a cleaning function to maintain cleanliness.
 
+let isPetDirty;
+let cleaningTimer;
+
 function cleanPet() {
-    
+    if (isPetDirty) {
+        // alert(`Your pet is dirty! Click the clean button to clean it.`);
+    } else {
+        // alert(`Your pet is now clean!`);
+
+        // reset the cleaning timer
+        clearTimeout(cleaningTimer);
+        // event listener for the "clean button"
+        const cleanButton = document.getElementById('cleanButton');
+        cleanButton.addEventListener('click', cleanPet);
+    }
 }
+
+function startCleaningTimer() {
+    // timer is set for 15 min
+    cleaningTimer = setTimeout(cleanPet, 1000);
+    isPetDirty = false;
+    console.log(cleaningTimer);
+}
+
+startCleaningTimer();
+console.log(isPetDirty);
